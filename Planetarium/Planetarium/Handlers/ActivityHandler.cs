2a89ac7fa238354c1d4dd163dad95362aa6c839b
DavidXie10 <davidxieli@gmail.com>
2021-09-22 15:08:01 -0600

    Adapted logo to home page and corrected ActivityModel class

--- Planetarium/Planetarium/Handlers/ActivityHandler.cs
+++ Planetarium/Planetarium/Handlers/ActivityHandler.cs
@@ -36,11 +36,10 @@ namespace Planetarium.Handlers {
             foreach (DataRow column in resultingTable.Rows) {
                 activities.Add(
                     new ActivityModel {
-                        title = Convert.ToString(column["titulo"]),
-                        body = Convert.ToString(column["cuerpo"]),
-                        id = Convert.ToInt32(column["id"]),
-                        img = Convert.ToInt32(column["img"]),
-                        date = Convert.ToString(column['date'])
+                        Title = Convert.ToString(column["titulo"]),
+                        Body = Convert.ToString(column["cuerpo"]),
+                        Id = Convert.ToInt32(column["id"]),
+                        Date = Convert.ToString(column["date"])
                     }
                 );
             }
--- Planetarium/Planetarium/Models/ActivityModel.cs
+++ Planetarium/Planetarium/Models/ActivityModel.cs
@@ -4,13 +4,10 @@ using System.Linq;
 using System.Web;
 
 namespace Planetarium.Models {
-
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Body { get; set; }
-    public string Date { get; set; }
-
     public class ActivityModel {
-
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Date { get; set; }
     }
 }
