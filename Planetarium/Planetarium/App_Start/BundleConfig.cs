608f6f36fcf67b97200345639aba10718b11e2c6
Carlos Espinoza <carlos.espinozaperaza@ucr.ac.cr>
2021-09-23 13:18:38 -0600

    footer updated

--- Planetarium/Planetarium/App_Start/BundleConfig.cs
+++ Planetarium/Planetarium/App_Start/BundleConfig.cs
@@ -28,7 +28,7 @@ namespace Planetarium
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
