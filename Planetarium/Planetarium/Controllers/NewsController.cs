46d9936d38147be6ac3a01e51ad15af6ef084ee7
solvalle <solvalle@github.com>
2021-09-27 18:11:59 -0600

    Adapted news MVC and handler

---
+++ Planetarium/Planetarium/Controllers/NewsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Planetarium.Handlers;
+using Planetarium.Models;
+
+namespace Planetarium.Controllers
+{
+    public class NewsController : Controller
+    {
+
+        public ActionResult ListNews()
+        {
+            NewsHandler dataAccess = new NewsHandler();
+            ViewBag.news = dataAccess.GetAllNews();
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            NewsModel news = new NewsModel
+            {
+                title = "Eclipse total de Sol este 2021",
+                category = "Cuerpos celestes",
+                topic = "Estrellas",
+                date = "01 / 12 / 2021",
+                description = "Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." +
+                "Neque porro quisquam est,qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit," +
+                "sed quia non numquam.Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Neque" +
+                " porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam. " +
+                "Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." +
+                "Neque porro quisquam est,qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit," +
+                "sed quia non numquam.Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Neque" +
+                " porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam. " +
+                "Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." +
+                "Neque porro quisquam est,qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit," +
+                "sed quia non numquam.Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Neque" +
+                " porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam. "
+            };
+            ViewBag.news = news;
+            return View();
+        }
+    }
+}
---
+++ Planetarium/Planetarium/Handlers/NewsHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+using Planetarium.Models;
+
+namespace Planetarium.Handlers
+{
+    public class NewsHandler
+    {
+        private SqlConnection connection;
+        private string connectionRoute;
+
+        public NewsHandler()
+        {
+            connectionRoute = ConfigurationManager.ConnectionStrings["AGREGAR EN WEB.CONF"].ToString();
+            connection = new SqlConnection(connectionRoute);
+        }
+
+        private DataTable CreateTableFromQuery(string query)
+        {
+            SqlCommand queryCommand = new SqlCommand(query, connection);
+            SqlDataAdapter tableAdapter = new SqlDataAdapter(queryCommand);
+            DataTable queryTable = new DataTable();
+            connection.Open();
+            tableAdapter.Fill(queryTable);
+            connection.Close();
+            return queryTable;
+        }
+
+        public List<NewsModel> GetAllNews()
+        {
+            List<NewsModel> news = new List<NewsModel>();
+            string query = "SELECT * FROM Noticia ORDER BY fechaDePublicacion DESC";
+            DataTable resultingTable = CreateTableFromQuery(query);
+            foreach (DataRow column in resultingTable.Rows)
+            {
+                news.Add(
+                    new NewsModel
+                    {
+                        title = Convert.ToString(column["titulo"]),
+                        category = Convert.ToString(column["categoria"]),
+                        topic = Convert.ToString(column["topico"]),
+                        date = Convert.ToString(column["fechaDePublicacion"]),
+                        description = Convert.ToString(column["resumen"]),
+
+                    }
+                );
+            }
+
+            return news;
+        }
+    }
+}
