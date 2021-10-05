224892f50e9475ef0849d4f373109f11ad2d5d5c
MauricioRojas17 <maurojseg17@gmail.com>
2021-10-05 12:50:23 -0600

    template for the activities approbation

---
+++ Planetarium/Planetarium/Handlers/CollaboratorHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using Planetarium.Models;
+
+namespace Planetarium.Handlers
+{
+    public class CollaboratorHandler
+    {
+        private SqlConnection connection;
+        private string connectionRoute;
+
+        public CollaboratorHandler()
+        {
+            connectionRoute = ConfigurationManager.ConnectionStrings["PlanetariumConnection"].ToString();
+            connection = new SqlConnection(connectionRoute);
+        }
+
+        private DataTable CreateTableFromQuery(string query)
+        {
+            SqlCommand queryCommand = new SqlCommand(query, connection);
+            SqlDataAdapter tableAdapter = new SqlDataAdapter(queryCommand);
+            DataTable queryTable = new DataTable();
+            connection.Open();
+            tableAdapter.Fill(queryTable);
+            connection.Close();
+            return queryTable;
+        }
+
+    }
+}
---
+++ Planetarium/Planetarium/Models/CollaboratorModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Planetarium.Models
+{
+    public class CollaboratorModel
+    {
+       
+
+    }
+}
