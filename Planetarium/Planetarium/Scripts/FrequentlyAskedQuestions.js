9ef041a6d10ce490e177827568f6a807e28a82dd
Carlos Espinoza <carlos.espinozaperaza@ucr.ac.cr>
2021-10-15 12:37:32 -0600

    MultiSelect refactored

--- Planetarium/Planetarium/Scripts/FrequentlyAskedQuestions.js
+++ Planetarium/Planetarium/Scripts/FrequentlyAskedQuestions.js
@@ -1,85 +1,36 @@
-﻿$(document).ready(function () {
-    $("#Category").change(function () {
-        $("#Topics").empty();
-        $("#selectedTopics").empty();
-        $("#inputTopicString").val("");
+﻿////$(document).ready(function () {
+////    $("#Category").change(function () {
+////        $("#Topics").empty();
+////        $("#selectedTopics").empty();
+////        $("#inputTopicString").val("");
 
-        if ($("#Category").val() != '') {
-            $.ajax({
+////        if ($("#Category").val() != '') {
+////            $.ajax({
 
-                type: 'POST',
-                url: $("#controllerURL").data("request-url"),
-                dataType: 'json',
+////                type: 'POST',
+////                url: $("#controllerURL").data("request-url"),
+////                dataType: 'json',
 
-                data: { category: $("#Category").val() },
+////                data: { category: $("#Category").val() },
 
-                success: function (topics) {
-                    $("#Topics").append('<option value="" >' + "---Seleccione un topico---" + '</option>');
-                    $.each(topics, function (i, topic) {
-                        $("#Topics").append('<option value=' + topic.Value.replace(" ", "_") + '>' + topic.Text + '</option>');
-                    });
+////                success: function (topics) {
+////                    $("#Topics").append('<option value="" >' + "---Seleccione un topico---" + '</option>');
+////                    $.each(topics, function (i, topic) {
+////                        $("#Topics").append('<option value=' + topic.Value.replace(" ", "_") + '>' + topic.Text + '</option>');
+////                    });
 
-                },
+////                },
 
-                error: function (ex) {
-                    alert('Fallo en la recuperación de tópicos' + ex);
-                }
+////                error: function (ex) {
+////                    alert('Fallo en la recuperación de tópicos' + ex);
+////                }
 
-            });
-            return false;
-        } else {
-            $("#Topics").empty();
-        }
-    }
+////            });
+////            return false;
+////        } else {
+////            $("#Topics").empty();
+////        }
+////    }
 
-    )
-});
-
-
-$(document).ready(function () {
-    $("#Topics").change(function () {
-        if ($("#Topics").val() != '')
-            addTopicButton($("#Topics").val());
-            addElementToArray($("#Topics").val());
-    })
-});
-
-function addTopicButton(value) {
-    let buttonsCard = document.querySelector("#selectedTopics")
-    let button = document.createElement("a");
-    button.textContent = String(value).replace("_", " ");
-    button.href = "#";
-    button.classList.add("btn", "btn-success");
-    button.style.margin = "2px";
-    button.addEventListener("click", (event) => {
-        let thisButton = event.target;
-        deleteTopicString(thisButton.textContent.replace(" ", "_"));
-        thisButton.parentNode.removeChild(thisButton);
-    })
-    buttonsCard.appendChild(button)
-}
-
-function addElementToArray(element) {
-    console.log(element);
-    let inputTopicString = document.querySelector("#inputTopicString");
-    console.log(inputTopicString);
-    let values = inputTopicString.value;
-    console.log("Prev: " + values)
-    if (values.includes(element) == 0) {
-        console.log("No se encuentra")
-        values += element + "|";
-    } else {
-        console.log("Se encuentra")
-    }
-    inputTopicString.value = values;
-    console.log("Inner: " + inputTopicString.value);
-}
-
-function deleteTopicString(topic) {
-    let topicString = document.querySelector("#inputTopicString");
-    let currentValue = topicString.value;
-    console.log("Current Values: " + currentValue)
-    let newValues = currentValue.replace(topic + "|", "");
-    console.log("New values: " + newValues);
-    topicString.value = newValues;
-}
+////    )
+////});
