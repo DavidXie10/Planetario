adb6a7dbc9c8bb2565f8eaf32579ade2fd3383d2
solvalle <vallesol07@gmail.com>
2021-11-10 01:18:29 -0600

    HU10: implemented first version of streamings
    Co-authored-by: mauricioRojasSegnini <mauricio.rojassegnini@ucr.ac.cr>

---
+++ Planetarium/Planetarium/Controllers/StreamingController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planetarium.Controllers {
+    public class StreamingController : Controller {
+
+        public ActionResult Streaming() {
+
+            return View();
+        }
+    }
+}
