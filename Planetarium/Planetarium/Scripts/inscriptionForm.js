dce94c35ccf79ea057a1cbec513ce1929a7113ae
DavidXie10 <davidxieli@gmail.com>
2021-11-14 18:21:29 -0600

    General: Extracted inscription form script
    Co-authored-by: Juan-Ignacio-Pacheco-Castro <juanipacas@gmail.com>

---
+++ Planetarium/Planetarium/Scripts/inscriptionForm.js
@@ -0,0 +1,7 @@
+﻿let currentDate = new Date();
+let month = currentDate.getMonth() + 1;
+let currentDay = currentDate.getDate();
+let day = currentDay / 10 >= 1 ? '' : '0';
+day += currentDay;
+document.getElementById("DateOfBirth").setAttribute("max", currentDate.getFullYear() + "-" + month + "-" + day);
+document.getElementById("DateOfBirth").setAttribute("class", "form-control");
