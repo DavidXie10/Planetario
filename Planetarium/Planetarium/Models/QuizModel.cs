3a67eb3d6220c1c590bcf0747859d08b57c85106
MauricioRojas17 <maurojseg17@gmail.com>
2021-11-14 19:04:22 -0600

    General. Diseño general: Aplicar conocimientos de UX en las vistas del planetario

--- Planetarium/Planetarium/Models/QuizModel.cs
+++ Planetarium/Planetarium/Models/QuizModel.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Planetarium.Models {
     public class QuizModel {
 
+        [Display(Name = "Título")]
         public string Titulo { get; set; }
+        [Display(Name = "Descripción")]
         public string Descripcion { get; set; }
         public string Dificultad { get; set; }
         public string Link { get; set; }
