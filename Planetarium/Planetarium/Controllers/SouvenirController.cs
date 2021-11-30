bddda53ac8ca8ec321088f1a94ffec1855786002
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-11-29 21:51:40 -0600

    HU2: Added SouvenirController
    Co-authored-by: solvalle <vallesol07@gmail.com>

---
+++ Planetarium/Planetarium/Controllers/SouvenirController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Planetarium.Handlers;
+
+namespace Planetarium.Controllers {
+    public class SouvenirController : Controller  {
+        SouvenirHandler souvenirHandler { get; set; }
+
+        public SouvenirController() {
+            souvenirHandler = new SouvenirHandler();
+        }
+
+        public ActionResult Catalog() {
+            ViewBag.Catalog = souvenirHandler.GetAllItems();
+            return View();
+        }
+    }
+}
