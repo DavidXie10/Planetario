7ba50ac4415b3ae2f0b7e89c521a5a2c9a14ba03
MauricioRojas17 <maurojseg17@gmail.com>
2021-12-11 05:23:25 -0600

    HU9. Implementation of a rate us section in the success view

---
+++ Planetarium/Planetarium/Controllers/RateUSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planetarium.Controllers
+{
+    public class RateUSController : Controller
+    {
+        // GET: RateUS
+        public ActionResult SuccessProcessRate()
+        {
+            return View();
+        }
+    }
+}
