2520296b3fe81cf520df6a6379a3ecfc5b20c5d8
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-11 15:38:54 -0600

    Tests: Installed Selenium
    Co-authored-by: DavidXie10 <davidxieli@gmail.com>

---
+++ Planetarium/UITestsPlanetarium/Controllers/UITest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestLab7.UITesting {
+    [TestClass]
+    public class Selenium {
+        IWebDriver driver;
+        [TestMethod]
+        public void PruebaIngresoCrearPlanetas() {
+            ///Arrange
+            /// Crea el driver de Chrome
+            driver = new ChromeDriver();
+            string URL = "*******la url de su apliación**********";
+
+            /// Pone la pantalla en full screen
+            driver.Manage().Window.Maximize();
+            ///Act
+            /// Se va a la URL indicada
+            driver.Url = URL;
+            ///Assert
+        }
+    }
+}
---
+++ Planetarium/UITestsPlanetarium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UITestsPlanetarium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UITestsPlanetarium")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa920d30-8d5d-439a-8cc3-1c19632b76e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
