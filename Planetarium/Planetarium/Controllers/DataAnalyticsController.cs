b6edb22a2c0037575ff8ea2f7e03bb52c1c08fd2
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-10 22:27:34 -0600

    HU7: Implemented Model, Handler and Controller for Item and Ticket

--- Planetarium/Planetarium/Controllers/DataAnalyticsController.cs
+++ Planetarium/Planetarium/Controllers/DataAnalyticsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Planetarium.Handlers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,40 @@ namespace Planetarium.Controllers
 {
     public class DataAnalyticsController : Controller
     {
-        // GET: DataAnalytics
-        public ActionResult StoreResports()
-        {
+        public DataAnalyticsHandler AnalyticsAccess { get; set; }
+
+        public DataAnalyticsController() {
+            AnalyticsAccess = new DataAnalyticsHandler();
+        }
+
+        public ActionResult StoreItemReport() {
+            DateTime end = new DateTime(2100,12,31);
+            DateTime begin = new DateTime(2000, 1, 1);
+            ViewBag.ItemReport = AnalyticsAccess.GetItemReport(begin, end);
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult ItemReportByDate(DateTime begin, DateTime end) {
+            ActionResult view = RedirectToAction("DataAnalytics", "StoreReports");
+
+            ViewBag.ItemReport = AnalyticsAccess.GetItemReport(begin, end);
+            return view;
+        }
+
+        public ActionResult StoreTicketReport() {
+            DateTime end = new DateTime(2100, 12, 31);
+            DateTime begin = new DateTime(2000, 1, 1);
+            ViewBag.ItemReport = AnalyticsAccess.GetTicketReport(begin, end);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TicketReportByDateEvent(DateTime begin, DateTime end) {
+            ActionResult view = RedirectToAction("DataAnalytics", "StoreReports");
+
+            ViewBag.ItemReport = AnalyticsAccess.GetTicketReport(begin, end);
+            return view;
         }
     }
 }
---
+++ Planetarium/Planetarium/Handlers/DataAnalyticsHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using Planetarium.Models;
+
+namespace Planetarium.Handlers {
+    public class DataAnalyticsHandler : DatabaseHandler {
+        public DataAnalyticsHandler() {
+            connectionRoute = ConfigurationManager.ConnectionStrings[CONNECTION_NAME].ToString();
+            connection = new SqlConnection(connectionRoute);
+        }
+
+        public List<ItemModel> GetItemReport(DateTime begin, DateTime end) {
+            List<ItemModel> items = new List<ItemModel>();
+            string query = "EXEC USP_obtenerVentasArticulos '" + begin + "','" + end + "'";
+            DataTable resultingTable = CreateTableFromQuery(query);
+            foreach (DataRow column in resultingTable.Rows) {
+                items.Add(
+                    new ItemModel {
+                        ID = Convert.ToInt32(column["ID"]),
+                        Name = Convert.ToString(column["Nombre"]),
+                        QuantitySold = Convert.ToInt32(column["Cantidad Vendida"]),
+                        LastBoughtDate = Convert.ToDateTime(column["Ultima fecha de compra"]),
+                        Category = Convert.ToString(column["Tipo de producto"]),
+                        Income = Convert.ToDouble(column["Ingresos"])
+                    });
+            }
+            return items;
+        }
+
+        public List<TicketModel> GetTicketReport(DateTime begin, DateTime end) {
+            List<TicketModel> items = new List<TicketModel>();
+            string query = "EXEC USP_obtenerVentasEntradas '" + begin + "','" + end + "'";
+
+            DataTable resultingTable = CreateTableFromQuery(query);
+            foreach (DataRow column in resultingTable.Rows) {
+                items.Add(
+                    new TicketModel {
+                        ActivityTitle = Convert.ToString(column["Nombre"]),
+                        QuantitySold = Convert.ToInt32(column["Cantidad Vendida"]),
+                        StartActivityDay = Convert.ToDateTime(column["Ultima fecha de compra"]),
+                        Income = Convert.ToDouble(column["Ingresos"])
+                    });
+            }
+            return items;
+        }
+
+    }
+}
