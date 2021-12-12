6fb889dd31ca4ba95c00eab53f46e9d7e5a49256
Carlos Espinoza <carlos.espinozaperaza@ucr.ac.cr>
2021-12-12 03:40:35 -0600

    HU5: Typing Legends working without modal

--- Planetarium/Planetarium/Scripts/TypingLegends.js
+++ Planetarium/Planetarium/Scripts/TypingLegends.js
@@ -2,19 +2,49 @@
 
 class TypingLegends {
     constructor(inputIds) {
-        console.log("se crea el juego")
+
+        //Input Containers
         this.textContainer = document.querySelector(inputIds[0]);
         this.userEntryContainer = document.querySelector(inputIds[1]);
         this.statusContainer = document.querySelector(inputIds[2]);
+        
+        //Timer Values
+        this.seconds = 0;
+        this.minutes = 0;
+        this.timerActive = false;
+        this.timerInterval = "";
+
+        //Global Texts
+        this.textOptions = [
+            "Neptuno es el octavo planeta en distancia respecto al Sol y el más lejano del sistema solar. Forma parte de los denominados planetas exteriores, y dentro de estos, es uno de los gigantes helados, y es el primero que fue descubierto gracias a predicciones matemáticas.",
+            "Saturno es el sexto planeta del sistema solar contando desde el Sol, el segundo en tamaño y masa después de Júpiter y el único con un sistema de anillos visible desde la Tierra. Su nombre proviene del dios romano Saturno. Forma parte de los denominados planetas exteriores o gaseosos.",
+            "Un anillo planetario es un anillo de polvo y otras partículas pequeñas que gira alrededor de un planeta.1​ Los más espectaculares y conocidos desde la época telescópica son los anillos de Saturno. Durante mucho tiempo se pensó que Saturno era el único planeta con anillos y su singularidad era un problema.",
+            "Júpiter es un cuerpo masivo gaseoso, formado principalmente por hidrógeno y helio, carente de una superficie interior definida. Entre los detalles atmosféricos es notable la Gran Mancha Roja (un enorme anticiclón situado en las latitudes tropicales del hemisferio sur), la estructura de nubes en bandas oscuras y zonas brillantes, y la dinámica atmosférica global determinada por intensos vientos zonales alternantes en latitud y con velocidades de hasta 140 m/s (504 km/h)."
+        ]
+
+        //Initialize Game
         this.startGame();
+
     }
 
     startGame() {
-        this.textContainer.textContent = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur";
-        this.userEntryContainer.placeholder = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur";
+        this.changeText();
     }
 
+    changeText() {
+        let randomOption = Math.floor(Math.random() * this.textOptions.length);
+        this.textContainer.textContent = this.textOptions[randomOption];
+        this.userEntryContainer.placeholder = this.textOptions[randomOption];
+    }
+
+
     checkChar() {
+
+        if (!this.timerActive) {
+            this.startTimer();
+            this.timerActive = false;
+        }
+
         let value = this.userEntryContainer.value;
         let textValue = this.textContainer.textContent;
         if (value[value.length - 1] == textValue[value.length - 1]) {
@@ -24,6 +54,9 @@ class TypingLegends {
             console.log("Se las pelo");
             this.changeToBadText();
         }
+
+        let progress = (value.length * 100 / textValue.length);
+        return progress;
     }
 
     changeToGoodText() {
@@ -45,6 +78,29 @@ class TypingLegends {
             statusClasses.remove("bg-success");
             statusClasses.add("bg-danger");
         }
+    }
+
+    startTimer() {
+        this.timerInterval = setInterval(() => {
+            let returnTime = this.timer(this.minutes, this.seconds);
+            this.minutes = returnTime[0];
+            this.seconds = returnTime[1];
+        }, 1000);
+    }
+
+    stopTimer() {
+        clearInterval(this.timerInterval);
+    }
+
+    timer(minutes, seconds) {
+        if (seconds + 1 == 60) {
+            seconds = 0;
+            minutes++;
+        } else {
+            seconds += 1;
+        }
+
+        return [minutes, seconds];
     }
 }
 
