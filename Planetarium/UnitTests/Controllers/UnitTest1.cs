db921d85eb572cb483502d5a84f74c03a696b431
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-11 21:02:52 -0600

    Tests: Added dependencies to start unit testing
    Co-authored-by: CarlosE2001 <carlosespinoza400@gmail.com>
    Co-authored-by: DavidXie10 <davidxieli@gmail.com>

---
+++ Planetarium/UnitTests/Controllers/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Planetarium.Controllers;
+
+namespace Planetarium.Tests.Controllers {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            Assert.AreEqual("hola", "hola");
+        }
+    }
+}
---
+++ Planetarium/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("504d0f71-c82e-4fc1-9583-fb05d3f237c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
