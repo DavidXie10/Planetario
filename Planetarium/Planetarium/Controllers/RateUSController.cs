1f6224c39ca0da8abc03549e740a5193c991e234
MauricioRojas17 <maurojseg17@gmail.com>
2021-12-12 22:36:57 -0600

    HU10: Implementation of UX results
    Co-authored-by: DavidXie10 <davidxieli@gmail.com>

--- Planetarium/Planetarium/Controllers/RateUSController.cs
+++ Planetarium/Planetarium/Controllers/RateUSController.cs
@@ -9,7 +9,6 @@ namespace Planetarium.Controllers
 {
     public class RateUSController : Controller
     {
-        // GET: RateUS
         public ActionResult SuccessProcessRate()
         {
             return View();
@@ -37,14 +36,87 @@ namespace Planetarium.Controllers
             return view;
         }
 
+        [HttpPost]
+        public ActionResult SubmitProcessesRate() {
+            ActionResult view = RedirectToAction("Index", "Home");
+            string emojiChoice = Request.Form["rateChoice"];
+            
+            /*
+            TxtContentParser txtParser = new TxtContentParser();
+            string fileContent = txtParser.ExtractRawContent(filename);
+            fileContent += emojiChoice + ",";
+            txtParser.WriteToFile(fileContent, filename);
+            */
+            return view;
+        }
+
         public ActionResult UXEvaluation() {
             ViewBag.Link= "https://docs.google.com/forms/d/e/1FAIpQLScpghh7KECEEjpnpJHqG1l9Zr2a4gcnDCpcKVpN1C2xt1ZMHw/viewform?embedded=true";
             return View();
         }
 
-        public ActionResult RateResults()
-        {
+        public ActionResult RateResults() {
+            string filename = "WebsiteRate.txt";
+            TxtContentParser txtParser = new TxtContentParser();
+            string fileContent = txtParser.ExtractRawContent(filename);
+            string[] evaluations = fileContent.Split(',');
+
+            //star survey
+            ViewBag.StarTotalResult = CountTotalElements(evaluations);
+            SetResultsOfStars(evaluations);
+            SetStarPercentages();
+
+            //faces survey
+            ViewBag.ExcelentResult = 0;
+            ViewBag.VeryGoodResult = 0;
+            ViewBag.GoodResult = 0;
+            ViewBag.FineResult = 0;
+            ViewBag.BadResult = 0;
+            ViewBag.ReallyBadResult = 0;
             return View();
         }
+
+        private int CountEvaluation(string value, string[] choices) {
+            int counter = 0;
+            foreach(string choice in choices) {
+                if(choice == value) {
+                    ++counter;
+                }
+            }
+
+            return counter;
+        }
+
+        private int CountTotalElements(string[] choices) {
+            int counter = 0;
+            foreach (string choice in choices) {
+                if (choice != "") {
+                    ++counter;
+                }
+            }
+
+            return counter;
+        }
+
+        private int GetPercentage(int starCounter, int totalStars) {
+            return starCounter * 100 / totalStars;
+        }
+
+        private void SetResultsOfStars(string[] evaluations) {
+            ViewBag.FiveStarResult = CountEvaluation("5", evaluations);
+            ViewBag.FourStarResult = CountEvaluation("4", evaluations);
+            ViewBag.ThreeStarResult = CountEvaluation("3", evaluations);
+            ViewBag.TwoStarResult = CountEvaluation("2", evaluations);
+            ViewBag.OneStarResult = CountEvaluation("1", evaluations);
+        }
+
+        private void SetStarPercentages() {
+            ViewBag.FiveStarPercentage = GetPercentage(ViewBag.FiveStarResult, ViewBag.StarTotalResult);
+            ViewBag.FourStarPercentage = GetPercentage(ViewBag.FourStarResult, ViewBag.StarTotalResult);
+            ViewBag.ThreeStarPercentage = GetPercentage(ViewBag.ThreeStarResult, ViewBag.StarTotalResult);
+            ViewBag.TwoStarPercentage = GetPercentage(ViewBag.TwoStarResult, ViewBag.StarTotalResult);
+            ViewBag.OneStarPercentage = GetPercentage(ViewBag.OneStarResult, ViewBag.StarTotalResult);
+        }
+
     }
 }
