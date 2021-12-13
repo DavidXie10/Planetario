b2d9a74f4b56ee69781c4dfcf2b67abb9c34ea12
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-13 04:29:08 -0600

    Minor fix

--- Planetarium/Planetarium/Scripts/payMethodEducationalActivity.js
+++ Planetarium/Planetarium/Scripts/payMethodEducationalActivity.js
@@ -1,24 +1,32 @@
 ﻿function checkDataEducationalActivity() {
 
-    let valid = true;
 
     let userName = document.getElementById("fname").value;
     let userLastName = document.getElementById("lname").value;
     let userMailName = document.getElementById("email").value;
     let cardNumber = document.getElementById("creditCardNumber").value;
+    let cardName = document.getElementById("creditCardName").value;
     let month = document.getElementById("monthInput").value;
     let year = document.getElementById("yearInput").value;
     let cvc = document.getElementById("cvcInput").value;
+    let typeCard = document.getElementById("typeCard");
+    let privacy = document.getElementById("privacy").checked;
 
-    valid = checkValidInputEducationalActivity(userName, "#nameEror", "Por favor ingrese un nombre");
-    valid = checkValidInputEducationalActivity(userLastName, "#lastNameError", "Ingrese un DNI");
-    valid = checkValidInputEducationalActivity(userMailName, "#emailError", "Ingrese un correo");
-    valid = checkValidInputEducationalActivity(cardNumber, "#cardNumberError", "Ingrese un número de tarjeta");
-    valid = checkValidInputEducationalActivity(month, "#monthError", "Ingrese un mes válido");
-    valid = checkValidInputEducationalActivity(year, "#yearError", "Ingrese un año");
-    valid = checkValidInputEducationalActivity(cvc, "#cvcError", "Ingrese el cvc al que se encuentra al reverso de su tarjeta");
+    let selectedTypeCard = typeCard.options[typeCard.selectedIndex].text;
 
-    if (valid) {
+    let nameValid = checkValidInputEducationalActivity(userName, "#nameEror", "Por favor ingrese un nombre");
+    let lastNameValid = checkValidInputEducationalActivity(userLastName, "#lastNameError", "Ingrese un DNI");
+    let emailValid = checkValidInputEducationalActivity(userMailName, "#emailError", "Ingrese un correo");
+    let cardNumberValid = checkValidInput(cardNumber, "#cardNumberError", "Ingrese un número de tarjeta");
+    let cardNameValid = checkValidInput(cardName, "#creditCardNameError", "Ingrese el nombre del tarjeta habiente");
+    let monthValid = checkValidInput(month, "#monthError", "Ingrese un mes válido");
+    let yearValid = checkValidInput(year, "#yearError", "Ingrese un año");
+    let cvcValid = checkValidInput(cvc, "#cvcError", "Ingrese el cvc");
+    let selectedTypeCardValid = checkValidInput(selectedTypeCard, "#typeCardError", "Seleccione el tipo de tarjeta");
+    let privacyValid = checkValidInput(privacy, "#privacyError", "Acepte las políticas de privacidad");
+
+
+    if (nameValid && lastNameValid && emailValid && cardNumberValid && cardNameValid && monthValid && yearValid && cvcValid && selectedTypeCardValid && privacyValid) {
         document.getElementById("submit").click();
     }
 }
