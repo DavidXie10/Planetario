3aca56b861eeded247235a8dc9a9260394327f30
DavidXie10 <davidxieli@gmail.com>
2021-12-13 21:48:35 -0600

    Tests: Implemented assign seats pass title to viewbag is correct test

---
+++ Planetarium/UnitTests/Controllers/EducationalActivityControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Planetarium.Controllers;
+
+namespace UnitTests.Controllers {
+    [TestClass]
+    public class EducationalActivityControllerTests {
+        [TestMethod]
+        public void TestAssignSeatActivityTitleIsCorrect() {
+            string activityTitle = "Joyas del Firmamento";
+            string activityDate = "2021-10-01";
+            EducationalActivityController educationalActivityController = new EducationalActivityController();
+
+            ViewResult view = educationalActivityController.AssignSeat("0", activityTitle, activityDate) as ViewResult;
+
+            Assert.AreEqual(activityTitle, view.ViewBag.title);
+        }
+    }
+}
